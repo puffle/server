bf40027155a83fe74163205983c957c43d47e222
Nobody <17956512+n0bodysec@users.noreply.github.com>
2023-07-22 08:14:50 -0300

    feat: add update_flooring event
    refactor: disable fixSync

--- src/plugins/game/Igloo.ts
+++ src/plugins/game/Igloo.ts
@@ -3,7 +3,6 @@ import { GameWorld } from '../../classes/GameWorld';
 import { User } from '../../classes/User';
 import { Igloo } from '../../classes/room/Igloo';
 import { MyAjv } from '../../managers/AjvManager';
-import { Config } from '../../managers/ConfigManager';
 import { IGamePlugin } from '../../types/types';
 import { constants } from '../../utils/constants';
 import { getIglooId } from '../../utils/functions';
@@ -12,6 +11,7 @@ import { GamePlugin } from '../GamePlugin';
 interface IAddIglooOrGetIglooOpenArgs { igloo: number; }
 interface IAddFurnitureArgs { furniture: number; }
 interface IUpdateIglooArgs { type: number; }
+interface IUpdateFlooringArgs { flooring: number; }
 interface IUpdateMusicArgs { music: number; }
 
 export default class IglooPlugin extends GamePlugin implements IGamePlugin
@@ -27,6 +27,7 @@ export default class IglooPlugin extends GamePlugin implements IGamePlugin
 			add_furniture: this.addFurniture,
 
 			update_igloo: this.updateIgloo,
+			update_flooring: this.updateFlooring,
 			update_music: this.updateMusic,
 
 			open_igloo: this.openIgloo,
@@ -63,6 +64,15 @@ export default class IglooPlugin extends GamePlugin implements IGamePlugin
 					type: { type: 'integer', minimum: 0 },
 				},
 			} as JSONSchemaType<IUpdateIglooArgs>)],
+
+			['updateFlooring', MyAjv.compile({
+				type: 'object',
+				additionalProperties: false,
+				required: ['flooring'],
+				properties: {
+					flooring: { type: 'integer', minimum: 0 },
+				},
+			} as JSONSchemaType<IUpdateFlooringArgs>)],
 
 			['updateMusic', MyAjv.compile({
 				type: 'object',
@@ -122,6 +132,26 @@ export default class IglooPlugin extends GamePlugin implements IGamePlugin
 		igloo.refresh(user);
 	};
 
+	updateFlooring = async (args: IUpdateFlooringArgs, user: User) =>
+	{
+		if (!this.schemas.get('updateFlooring')!(args)) return;
+
+		const igloo = this.getIgloo(user.data.id);
+		if (igloo === undefined || igloo !== user.room || igloo.dbData.flooring === args.flooring) return;
+
+		const flooring = user.validatePurchase.flooring(args.flooring);
+		if (!flooring) return;
+
+		await igloo.dbUpdate({ flooring: args.flooring });
+		igloo.dbData.flooring = args.flooring;
+
+		await user.updateCoins(-flooring.cost);
+
+		// TODO: add fixSync
+
+		user.send('update_flooring', { flooring: args.flooring, coins: user.data.coins });
+	};
+
 	updateMusic = async (args: IUpdateMusicArgs, user: User) =>
 	{
 		if (!this.schemas.get('updateMusic')!(args)) return;
@@ -132,8 +162,10 @@ export default class IglooPlugin extends GamePlugin implements IGamePlugin
 		await igloo.dbUpdate({ music: args.music });
 		igloo.dbData.music = args.music;
 
-		if (Config.data.game.fixSync) user.sendRoom('update_music', { music: args.music }, []);
-		else user.send('update_music', { music: args.music });
+		// TODO: add fixSync
+		// if (Config.data.game.fixSync) user.sendRoom('update_music', { music: args.music }, []);
+
+		user.send('update_music', { music: args.music });
 	};
 
 	openIgloo = (args: unknown, user: User) =>
