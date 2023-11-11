a153450b869f45b7fe5e9d943bd9b3b7651ce576
Nobody <17956512+n0bodysec@users.noreply.github.com>
2023-11-11 12:29:50 -0300

    refactor: migrate ConfigManager
    chore: update dependencies

--- src/managers/ConfigManager.ts
+++ src/managers/ConfigManager.ts
@@ -1,6 +1,4 @@
-import { merge } from 'lodash';
-import { readFile } from 'node:fs/promises';
-import { join } from 'node:path';
+import { ConfigManager } from '@n0bodysec/config-manager';
 import { constants } from '../utils/constants';
 
 export type TConfig = {
@@ -51,102 +49,54 @@ export type TConfig = {
 	};
 };
 
-export class ConfigManager
-{
-	constructor(defaultData?: TConfig)
-	{
-		this.data = Object.create(defaultData ?? this.defaultData);
-	}
-
-	initialized = false;
-	data: TConfig;
-	defaultData: TConfig = {
-		logLevel: 'info',
-		crypto: {
-			secret: 'UNSECURE SECRET - DO NOT USE ME',
-			jwtExpiry: 600,
-			audience: 'localhost',
-			issuer: constants.PROJECT_NAME,
-			rounds: 10,
+export const defaultData: TConfig = {
+	logLevel: 'info',
+	crypto: {
+		secret: 'UNSECURE SECRET - DO NOT USE ME',
+		jwtExpiry: 600,
+		audience: 'localhost',
+		issuer: constants.PROJECT_NAME,
+		rounds: 10,
+	},
+	cors: {
+		origin: 'http://localhost:8080',
+	},
+	reverseProxy: {
+		enabled: false,
+		ipHeader: 'X-Forwarded-For',
+		separator: ',',
+	},
+	rateLimits: {
+		enabled: false,
+		connections: {
+			points: 5,
+			duration: 1,
 		},
-		cors: {
-			origin: 'http://localhost:8080',
+		messages: {
+			points: 50,
+			duration: 1,
 		},
-		reverseProxy: {
-			enabled: false,
-			ipHeader: 'X-Forwarded-For',
-			separator: ',',
-		},
-		rateLimits: {
-			enabled: false,
-			connections: {
-				points: 5,
-				duration: 1,
-			},
-			messages: {
-				points: 50,
-				duration: 1,
-			},
-		},
-		http: {
+	},
+	http: {
+		host: 'localhost',
+		port: 6111,
+	},
+	worlds: {
+		Blizzard: {
 			host: 'localhost',
-			port: 6111,
+			port: 6112,
+			maxUsers: 300,
 		},
-		worlds: {
-			Blizzard: {
-				host: 'localhost',
-				port: 6112,
-				maxUsers: 300,
-			},
-		},
-		game: {
-			preferredSpawn: 0,
-			iglooIdOffset: 2000,
-			fixSync: false,
-		},
-		login: {
-			tokenDuration: 30,
-			enableExpiration: true,
-		},
-	};
+	},
+	game: {
+		preferredSpawn: 0,
+		iglooIdOffset: 2000,
+		fixSync: false,
+	},
+	login: {
+		tokenDuration: 30,
+		enableExpiration: true,
+	},
+};
 
-	Initialize = async (path?: string) =>
-	{
-		await this.#load(path ?? join(__dirname, '..', '..', 'config', 'config.json'));
-		this.initialized = true;
-	};
-
-	#load = async (path: string) =>
-	{
-		const file = await readFile(path, 'utf-8');
-		this.data = JSON.parse(file);
-		this.sanitize();
-	};
-
-	sanitize = () =>
-	{
-		// sanitize the typeof data loaded from config
-		this.#sanitizeType(this.defaultData, this.data);
-
-		// merge data not found in the loaded config
-		this.data = merge(this.defaultData, this.data);
-	};
-
-	#sanitizeType = (src: Record<string, unknown>, dst: Record<string, unknown>) =>
-	{
-		Object.keys(dst).filter((key) => key in src).forEach((key) =>
-		{
-			const type1 = typeof dst[key];
-			const type2 = typeof src[key];
-
-			// check if we can actually read a default data to compare against
-			if (type2 !== 'undefined')
-			{
-				if (type1 === 'object' && src[key] != null) this.#sanitizeType(src[key] as Record<string, unknown>, dst[key] as Record<string, unknown>);
-				if (type1 !== type2) dst[key] = src[key];
-			}
-		});
-	};
-}
-
-export const Config = new ConfigManager();
+export const Config = new ConfigManager<TConfig>(defaultData);
